d3ebe5e0d0fa247a0c97d467d30bf2e69ea2ea13
Asia Kane and Steven Galvin <steven@>
2017-05-31 10:31:01 -0700

    create rental route as a dynamic segment and create rental-detail component to update and delete individual rentals

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -5,9 +5,19 @@ export default Ember.Route.extend({
     return this.store.findRecord('rental', params.rental_id)
   },
   actions: {
+    update(rental, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          rental.set(key,params[key]);
+        }
+      });
+      rental.save();
+      this.transitionTo('index');
+    },
+    
     destroyRental(rental) {
       rental.destroyRecord();
       this.transitionTo('index');
-    },
+    }
   }
 });
